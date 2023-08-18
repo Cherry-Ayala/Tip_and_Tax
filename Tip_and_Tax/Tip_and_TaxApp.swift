8f834d21e1169527b21bd0810038bb028c1f0ed6
Cherry-Ayala <cherry.devservices@gmail.com>
2023-08-18 12:28:00 -0600

    Initial Commit

---
+++ Tip_and_Tax/Tip_and_TaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_and_TaxApp.swift
+//  Tip_and_Tax
+//
+//  Created by Charlie Ayala on 18/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Tip_and_TaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tip_and_TaxUITests/Tip_and_TaxUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Tip_and_TaxUITestsLaunchTests.swift
+//  Tip_and_TaxUITests
+//
+//  Created by Charlie Ayala on 18/08/23.
+//
+
+import XCTest
+
+final class Tip_and_TaxUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
